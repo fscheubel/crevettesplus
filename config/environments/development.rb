f96e1ed7c976ab66d5fa9452e2d445135a7ad65a
Frederic Scheubel <f@migario.com>
2017-02-14 02:16:27 +0000

    Integrated favicons and silenced annoying warnings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Disables "Cannot render from console" warnings on cloud9
+  config.web_console.whiny_requests = false
 end
